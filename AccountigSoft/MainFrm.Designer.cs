ce4037c700e830ed147e8ea6e61637cf0861a89d
syvano <longmarchmotors@gmail.com>
2024-10-06 18:12:50 +0100

    added buttons and images to resources, assigned images to buttons

--- AccountigSoft/MainFrm.Designer.cs
+++ AccountigSoft/MainFrm.Designer.cs
@@ -29,8 +29,12 @@
         private void InitializeComponent()
         {
             topPanel = new Panel();
-            closeBtn = new Button();
-            minBtn = new Button();
+            HelpBtn = new Button();
+            ToolsBtn = new Button();
+            ReportsBtn = new Button();
+            AccountingBtn = new Button();
+            CloseBtn = new Button();
+            MinBtn = new Button();
             bottomPanel = new Panel();
             topPanel.SuspendLayout();
             SuspendLayout();
@@ -38,35 +42,97 @@
             // topPanel
             // 
             topPanel.BackColor = SystemColors.ScrollBar;
-            topPanel.Controls.Add(closeBtn);
-            topPanel.Controls.Add(minBtn);
+            topPanel.Controls.Add(HelpBtn);
+            topPanel.Controls.Add(ToolsBtn);
+            topPanel.Controls.Add(ReportsBtn);
+            topPanel.Controls.Add(AccountingBtn);
+            topPanel.Controls.Add(CloseBtn);
+            topPanel.Controls.Add(MinBtn);
             topPanel.Dock = DockStyle.Top;
             topPanel.Location = new Point(0, 0);
             topPanel.Name = "topPanel";
-            topPanel.Size = new Size(1310, 100);
+            topPanel.Size = new Size(1310, 92);
             topPanel.TabIndex = 0;
             // 
-            // closeBtn
+            // HelpBtn
             // 
-            closeBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            closeBtn.Location = new Point(1244, 19);
-            closeBtn.Name = "closeBtn";
-            closeBtn.Size = new Size(47, 36);
-            closeBtn.TabIndex = 1;
-            closeBtn.Text = "close";
-            closeBtn.UseVisualStyleBackColor = true;
-            closeBtn.Click += closeBtn_Click;
+            HelpBtn.BackgroundImage = Properties.Resources.butt_background;
+            HelpBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            HelpBtn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            HelpBtn.Image = Properties.Resources.top_Help_butt;
+            HelpBtn.Location = new Point(363, 10);
+            HelpBtn.Name = "HelpBtn";
+            HelpBtn.Size = new Size(85, 71);
+            HelpBtn.TabIndex = 5;
+            HelpBtn.Text = "Help F5";
+            HelpBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            HelpBtn.UseVisualStyleBackColor = true;
             // 
-            // minBtn
+            // ToolsBtn
             // 
-            minBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            minBtn.Location = new Point(1191, 19);
-            minBtn.Name = "minBtn";
-            minBtn.Size = new Size(47, 36);
-            minBtn.TabIndex = 0;
-            minBtn.Text = "min";
-            minBtn.UseVisualStyleBackColor = true;
-            minBtn.Click += minBtn_Click;
+            ToolsBtn.BackgroundImage = Properties.Resources.butt_background;
+            ToolsBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            ToolsBtn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ToolsBtn.Image = Properties.Resources.top_Tools_butt;
+            ToolsBtn.Location = new Point(264, 10);
+            ToolsBtn.Name = "ToolsBtn";
+            ToolsBtn.Size = new Size(93, 71);
+            ToolsBtn.TabIndex = 4;
+            ToolsBtn.Text = "Tools F4";
+            ToolsBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            ToolsBtn.UseVisualStyleBackColor = true;
+            // 
+            // ReportsBtn
+            // 
+            ReportsBtn.BackgroundImage = Properties.Resources.butt_background;
+            ReportsBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            ReportsBtn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ReportsBtn.Image = Properties.Resources.top_report_butt;
+            ReportsBtn.Location = new Point(145, 10);
+            ReportsBtn.Name = "ReportsBtn";
+            ReportsBtn.Size = new Size(113, 71);
+            ReportsBtn.TabIndex = 3;
+            ReportsBtn.Text = "Reports F3";
+            ReportsBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            ReportsBtn.UseVisualStyleBackColor = true;
+            // 
+            // AccountingBtn
+            // 
+            AccountingBtn.BackgroundImage = Properties.Resources.butt_background;
+            AccountingBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            AccountingBtn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AccountingBtn.Image = Properties.Resources.top_input_butt;
+            AccountingBtn.Location = new Point(12, 10);
+            AccountingBtn.Name = "AccountingBtn";
+            AccountingBtn.Size = new Size(127, 71);
+            AccountingBtn.TabIndex = 2;
+            AccountingBtn.Text = "Accounting F2";
+            AccountingBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            AccountingBtn.UseVisualStyleBackColor = true;
+            // 
+            // CloseBtn
+            // 
+            CloseBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            CloseBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            CloseBtn.Image = Properties.Resources._34217_close_delete_remove_icon;
+            CloseBtn.Location = new Point(1272, 11);
+            CloseBtn.Name = "CloseBtn";
+            CloseBtn.Size = new Size(20, 20);
+            CloseBtn.TabIndex = 1;
+            CloseBtn.UseVisualStyleBackColor = true;
+            CloseBtn.Click += closeBtn_Click;
+            // 
+            // MinBtn
+            // 
+            MinBtn.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            MinBtn.BackgroundImageLayout = ImageLayout.Stretch;
+            MinBtn.Image = Properties.Resources._623091_box_minimize_close_delete_minus_icon__2_;
+            MinBtn.Location = new Point(1249, 11);
+            MinBtn.Name = "MinBtn";
+            MinBtn.Size = new Size(20, 20);
+            MinBtn.TabIndex = 0;
+            MinBtn.UseVisualStyleBackColor = true;
+            MinBtn.Click += minBtn_Click;
             // 
             // bottomPanel
             // 
@@ -99,7 +165,11 @@
 
         private Panel topPanel;
         private Panel bottomPanel;
-        private Button closeBtn;
-        private Button minBtn;
+        private Button CloseBtn;
+        private Button MinBtn;
+        private Button AccountingBtn;
+        private Button ReportsBtn;
+        private Button HelpBtn;
+        private Button ToolsBtn;
     }
 }
--- AccountigSoft/obj/Debug/net8.0-windows/AccountigSoft.AssemblyInfo.cs
+++ AccountigSoft/obj/Debug/net8.0-windows/AccountigSoft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountigSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d732812d1662223daee0649169ca4c01864b88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3e01038eb99bd3f99143281399a738d0bec635")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountigSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountigSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
